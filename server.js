5bde25b0fbcf619f40d2fc986f8e824448a49b18
taroscript <80182875+taroscript@users.noreply.github.com>
2021-11-20 14:14:49 +0900

    simple

--- server.js
+++ server.js
@@ -12,26 +12,6 @@ const io = socketIO( server );
 
 // 定数
 const PORT = process.env.PORT || 3000;
-const SYSTEMNICKNAME = '**system**'
-
-// 関数
-// 数字を２桁の文字列に変換
-const toDoubleDigitString =
-    ( num ) =>
-    {
-        return ( "0" + num ).slice( -2 );   // slice( -2 )で後ろから２文字取り出す。
-    };
-
-// 時刻文字列の作成（書式は「YY/DD/MM hh:mm ss」）
-const makeTimeString =
-    ( time ) =>
-    {
-        return toDoubleDigitString( time.getFullYear() ) + '/' + toDoubleDigitString( time.getMonth() + 1 ) + '/' + toDoubleDigitString( time.getDate() )
-            + ' ' + toDoubleDigitString( time.getHours() ) + ':' + toDoubleDigitString( time.getMinutes() ) + ' ' + toDoubleDigitString( time.getSeconds() );
-    }
-
-// グローバル変数
-let iCountUser = 0; // ユーザー数
 
 // 接続時の処理
 // ・サーバーとクライアントの接続が確立すると、
@@ -43,86 +23,26 @@ io.on(
     {
         console.log( 'connection' );
 
-        let strNickname = '';	// コネクションごとで固有のニックネーム。イベントをまたいで使用される。
-
         // 切断時の処理
         // ・クライアントが切断したら、サーバー側では'disconnect'イベントが発生する
         socket.on(
             'disconnect',
             () =>
             {
-              console.log( 'disconnect' );
-
-              if( strNickname )
-              {
-                  // ユーザー数の更新
-                  iCountUser--;
-
-                  // メッセージオブジェクトに現在時刻を追加
-                  const strNow = makeTimeString( new Date() );
-
-                  // システムメッセージの作成
-                  const objMessage = {
-                      strNickname: SYSTEMNICKNAME,
-                      strMessage: strNickname + ' left.' + " there are " + iCountUser + " participants",
-                      strDate: strNow
-                  }
-
-                  // 送信元含む全員に送信
-                  io.emit( 'spread message', objMessage );
-              }
+                console.log( 'disconnect' );
             } );
-        
-        // 入室時の処理
-        // ・クライアント側のメッセージ送信時の「socket.emit( 'join', strNickname );」に対する処理
-        socket.on(
-          'join',
-          ( strNickname_ ) =>
-          {
-              console.log( 'joined :', strNickname_ );
-
-              // コネクションごとで固有のニックネームに設定
-              strNickname = strNickname_;
-
-              // ユーザー数の更新
-              iCountUser++;
-
-              // メッセージオブジェクトに現在時刻を追加
-              const strNow = makeTimeString( new Date() );
-
-              // システムメッセージの作成
-              const objMessage = {
-                  strNickname: SYSTEMNICKNAME,
-                  strMessage: strNickname + ' joined.' + " there are " + iCountUser + " participants",
-                  strDate: strNow
-              }
-
-              // 送信元含む全員に送信
-              io.emit( 'spread message', objMessage );
-          } );
 
         // 新しいメッセージ受信時の処理
         // ・クライアント側のメッセージ送信時の「socket.emit( 'new message', $( '#input_message' ).val() );」に対する処理
         socket.on(
-          'new message',
-          ( strMessage ) =>
-          {
-            console.log( 'new message', strMessage );
+            'new message',
+            ( strMessage ) =>
+            {
+                console.log( 'new message', strMessage );
 
-            // 現在時刻の文字列の作成
-            const strNow = makeTimeString( new Date() );
-
-            // メッセージオブジェクトの作成
-            const objMessage = {
-              strNickname: strNickname,
-              strMessage: strMessage,
-              strDate: strNow
-          }
-
-            // 送信元含む全員に送信
-            //io.emit( 'spread message', strMessage );
-            io.emit( 'spread message', objMessage );
-          } );
+                // 送信元含む全員に送信
+                io.emit( 'spread message', strMessage );
+            } );
     } );
 
 // 公開フォルダの指定
@@ -130,7 +50,7 @@ app.use( express.static( __dirname + '/public' ) );
 
 // サーバーの起動
 server.listen(
-    PORT, "0.0.0.0", 
+    PORT, "0.0.0.0",
     () =>
     {
         console.log( 'Server on port %d', PORT );
